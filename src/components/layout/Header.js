26f95fb1c26011827eb68550060b66488d17869e
Enocho1-1 <enoch.okyere@gmail.com>
2023-12-15 12:48:01 -0500

    header updates

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,54 @@
 
+import twitter from "../../assets/social_media/twitter.png"
+import youtube from "../../assets/social_media/youtube.png"
+import instagram from "../../assets/social_media/instagram.png"
+import linkedin from "../../assets/social_media/linkedin.png"
+import facebook from "../../assets/social_media/facebook.png"
 
 export const Header = () => {
+
+  const socials = [
+    {
+      img: twitter,
+      name: "X"
+    },
+    {
+      img: youtube,
+      name: "Youtube"
+    },
+    {
+      img: instagram ,
+      name: "Instagram"
+    },
+    {
+      img:  linkedin,
+      name: "Linkedin"
+    },
+    {
+      img: facebook,
+      name: "Facebook"
+    }
+
+  ]
   return (
-    <div className="sticky bg-gray-900 w-inherit">
-      <span className="bg-blue-500 py-4 h-[100px]"> 
-        <h1 className="text-2xl font-semibold">World News</h1>
-      </span>
+    <div className="sticky bg-gray-900 w-inherit h-[3.75rem] flex justify-between">
+      <header className="bg-blue-400 text-gray-900 px-2 h-inherit text-[2.813rem] flex items-center font-open_sans"> 
+        <h1 className="font-light">World</h1>
+        <p className="font-semibold">News.</p>
+      </header>
+
+      {/* Social Media */}
+      <aside className="h-inherit flex items-center mr-6">
+        <h1 className="text-gray-200 text-md font-medium">Join us</h1>
+    
+        <span className="flex">
+          { socials.map((item,index) => (
+            <div key={index}>
+              <img className="h-[20px] w-[20px] mx-2" src={item.img} alt="social_icon" title={item.name} />
+            </div>
+          ))}
+        </span>
+      </aside>
          
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        open_sans: ["Open Sans", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
