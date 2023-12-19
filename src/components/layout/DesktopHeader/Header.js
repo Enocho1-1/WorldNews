8dd51a6bd50fe35e209eea31699200c43a1c59f1
Enocho1-1 <enoch.okyere@gmail.com>
2023-12-19 10:54:34 -0500

    nav options export/import

--- src/components/layout/DesktopHeader/Header.js
+++ src/components/layout/DesktopHeader/Header.js
@@ -1,53 +1,10 @@
 /* eslint-disable */
 import { useState } from "react"
 import { HeaderTop,HeaderBottom } from "./components"
+import {navOptions} from "../../arrays/NavOptions"
 
 export const Header = () => {
 
-  const navOptions = [
-    { 
-      name:"NEWS",
-      color:"bg-dark_orange",
-      text: "text-dark_orange",
-      path: "/news"
-    },
-    { 
-      name:"BUSINESS",
-      color:"bg-vivid_cyan",
-      text: "text-vivid_cyan",
-      path: "/business"
-    },
-    { 
-      name:"SPORTS",
-      color:"bg-pure_green",
-      text: "text-pure_green",
-      path: "/sports"
-    },
-    { 
-      name:"ENTERTAINMENT",
-      color:"bg-bright_blue",
-      text: "text-bright_blue",
-      path: "/entertainment"
-    },
-    { 
-      name:"HEALTH",
-      color:"bg-pure_yellow",
-      text: "text-pure_yellow",
-      path: "/health"
-    },
-    { 
-      name:"SCIENCE",
-      color:"bg-pure_gray",
-      text: "text-pure_gray",
-      path: "/science"
-    },
-    { 
-      name:"TECHNOLOGY",
-      color:"bg-moderate_violet",
-      text: "text-moderate_violet",
-      path: "/tech"
-    }
-  ]
   const [category,setCategory] = useState(navOptions[0])
   const [search, setSearch] = useState(true)
 
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,58 +1,9 @@
 import { Route,Routes } from "react-router-dom"
 import { CategoryPage,Weather } from "../pages"
+import { navOptions } from "../components/arrays/NavOptions"
 
 export const AllRoutes = () => {
-  const navOptions = [
-    { 
-      name:"NEWS",
-      color:"bg-dark_orange",
-      text: "text-dark_orange",
-      border:"border-dark_orange",
-      path: "/news"
-    },
-    { 
-      name:"BUSINESS",
-      color:"bg-vivid_cyan",
-      text: "text-vivid_cyan",
-      border:"border-vivid_cyan",
-      path: "/business"
-    },
-    { 
-      name:"SPORTS",
-      color:"bg-pure_green",
-      text: "text-pure_green",
-      border:"border-pure_green",
-      path: "/sports"
-    },
-    { 
-      name:"ENTERTAINMENT",
-      color:"bg-bright_blue",
-      text: "text-bright_blue",
-      border:"border-bright_blue",
-      path: "/entertainment"
-    },
-    { 
-      name:"HEALTH",
-      color:"bg-pure_yellow",
-      text: "text-pure_yellow",
-      border:"border-pure_yellow",
-      path: "/health"
-    },
-    { 
-      name:"SCIENCE",
-      color:"bg-pure_gray",
-      text: "text-pure_gray",
-      border:"border-pure_gray",
-      path: "/science"
-    },
-    { 
-      name:"TECHNOLOGY",
-      color:"bg-moderate_violet",
-      text: "text-moderate_violet",
-      border:"border-moderate_violet",
-      path: "/tech"
-    }
-  ]
+
   return (
    <>
     <Routes>
