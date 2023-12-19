3dca53879795c38412ed5ef42f09f5cfb0e1990a
Enocho1-1 <enoch.okyere@gmail.com>
2023-12-19 12:12:26 -0500

    responsive design css

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -5,7 +5,7 @@ export const CategoryPage = ({option}) => {
     useTitle(name)
   return (
     <div className=" flex font-open_sans">
-        <section className="w-[1200px] mt-[90px] flex flex-col">
+        <section className="w-[1200px]  max-[720px]:mt-0 min-[720px]:max-[1340px]:mt-[120px] min-[1340px]:mt-[90px] flex flex-col">
             <header className={`w-inherit flex border-b-2 ${border}`}>
               <span className={`${color} h-[40px] w-[30px]`}></span>
               <h1 className="font-semibold text-xl ml-4 self-center">Latest</h1>
--- src/pages/Weather/Weather.js
+++ src/pages/Weather/Weather.js
@@ -3,7 +3,7 @@
 export const Weather = () => {
   return (
     <div className="flex font-open_sans">
-        <section className="w-[1200px] mt-[90px] flex flex-col">
+        <section className="w-[1200px] max-[720px]:mt-0 min-[720px]:max-[1340px]:mt-[120px] min-[1340px]:mt-[90px] flex flex-col">
             <header >
                 <h1 className="font-semibold text-xl ml-4 self-center">Weather</h1>
             </header>
