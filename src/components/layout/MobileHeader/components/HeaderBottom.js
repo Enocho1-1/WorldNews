752e6a3d29c415ab86d37a63d0be41072bbb7f4b
Enocho1-1 <enoch.okyere@gmail.com>
2023-12-19 11:56:32 -0500

    mobile side menu

--- src/components/layout/MobileHeader/components/HeaderBottom.js
+++ src/components/layout/MobileHeader/components/HeaderBottom.js
@@ -8,11 +8,11 @@ export const HeaderBottom = ({isHidden,setIsHidden}) => {
   return (
     <div className="relative h-[50px] w-[100%] flex font-open_sans">
       {/* Category Topic */}
-      <span className={`h-inherit w-auto px-2 bg-gray-900 flex justify-center items-center border-y-2 ${category.border}`}>
-        <h1 className={`${category.text} text-xl font-semibold`}>{category.name}</h1>
+      <span className={`h-inherit max-w-[35%] px-2 bg-gray-900 flex justify-center items-center border-y-2 ${category.border}`}>
+        <h1 className={`${category.text} text-lg font-semibold`}>{category.name}</h1>
       </span>
       {/* Just-In News */}
-      <button className="h-inherit w-[65%] flex justify-start items-center border-y-2 border-red-600">
+      <button className="h-inherit w-[100%] flex justify-start items-center border-y-2">
         <h1 className="ml-4 text-xl text-gray-600 font-semibold hover:text-black ">JUST IN</h1>
       </button>
 
