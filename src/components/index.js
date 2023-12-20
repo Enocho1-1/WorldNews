cd1e721cb9fb50576ee97d0c998859be9bbf039e
Enocho1-1 <enoch.okyere@gmail.com>
2023-12-20 09:29:47 -0500

    mobile just in component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { MobileHeader } from "./layout/MobileHeader/MobileHeader"
 export { Footer } from "./layout/Footer"
 
 export {JustIn} from "./elements/JustIn/JustIn"
+export {MobileJustIn} from "./elements/JustIn/MobileJustIn"
--- src/components/layout/MobileHeader/components/HeaderBottom.js
+++ src/components/layout/MobileHeader/components/HeaderBottom.js
@@ -1,23 +1,35 @@
 import { useState } from "react"
 import { SideMenu } from "./SideMenu"
 import { navOptions } from "../../../arrays/NavOptions"
+import { MobileJustIn } from "../../../elements/JustIn/MobileJustIn"
 
 export const HeaderBottom = ({isHidden,setIsHidden}) => {
   const [category,setCategory] = useState(navOptions[0])
-
+  const [hide,setHide] = useState(false)
   return (
-    <div className="relative h-[50px] w-[100%] flex font-open_sans">
-      {/* Category Topic */}
-      <span className={`h-inherit max-w-[35%] px-2 bg-gray-900 flex justify-center items-center border-y-2 ${category.border}`}>
-        <h1 className={`${category.text} text-lg font-semibold`}>{category.name}</h1>
-      </span>
-      {/* Just-In News */}
-      <button className="h-inherit w-[100%] flex justify-start items-center border-y-2">
-        <h1 className="ml-4 text-xl text-gray-600 font-semibold hover:text-black ">JUST IN</h1>
-      </button>
+    <div>
 
-      {/* SideMenu */}
-      { isHidden && <SideMenu setIsHidden={setIsHidden} setCategory={setCategory}/>}
+      <section className="relative h-[50px] w-[100%] flex font-open_sans">
+          {/* Category Topic */}
+
+          <span className={`h-inherit max-w-[35%] px-2 bg-gray-900 flex justify-center items-center border-y-2 ${category.border}`}>
+            <h1 className={`${category.text} text-lg font-semibold`}>{category.name}</h1>
+          </span>
+
+          {/* Just-In News */}
+
+          <button onClick={() => setHide(!hide)} className="h-inherit w-[100%] flex justify-start items-center border-y-2">
+            <h1 className="ml-4 text-xl text-gray-600 font-semibold hover:text-black ">JUST IN</h1>
+          </button>
+
+          {/* SideMenu */}
+
+          { isHidden && <SideMenu setIsHidden={setIsHidden} setCategory={setCategory}/>}
+      </section>
+
+      {/* Mobile Just In News */}
+      { hide && <MobileJustIn />}
     </div>
+  
   )
 }
