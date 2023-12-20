15f507b92a73b17d089a16a5bd4f33e3e3786f8a
Enocho1-1 <enoch.okyere@gmail.com>
2023-12-20 09:39:22 -0500

    mobile just in component

--- src/components/elements/JustIn/MobileJustIn.js
+++ src/components/elements/JustIn/MobileJustIn.js
@@ -1,7 +1,14 @@
-
+import { JustInNews } from "./components/JustInNews"
 
 export const MobileJustIn = () => {
+    const date = new Date()
   return (
-    <div>MobileJustIn</div>
+    <div className="w-[100%] flex flex-col" >
+        <header className="h-[2.625rem]  bg-gray-800 text-gray-200 text-xl py-4 font-medium flex justify-center items-center">Just In</header>
+        <span className="h-[2.188rem] flex justify-center items-center py-2 bg-gray-100 text-gray-200">
+            <p className="text-xs font-medium bg-gray-700 p-1 rounded-lg">{date.toDateString()}</p>
+        </span>
+        <JustInNews />
+    </div>
   )
 }
