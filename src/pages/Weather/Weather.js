de134955717c472960ea2b626d4765305e08a04f
Enocho1-1 <enoch.okyere@gmail.com>
2023-12-20 11:02:27 -0500

    get user longitude/latitude - weather comp

--- src/pages/Weather/Weather.js
+++ src/pages/Weather/Weather.js
@@ -2,11 +2,34 @@ import { useTitle } from "../../hooks"
 
 export const Weather = () => {
   useTitle("WEATHER")
+
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+  
+  function success(pos) {
+    const crd = pos.coords;
+  
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
+  
+
   return (
     <div className="flex font-open_sans">
         <section className="w-[1200px] max-[720px]:mt-0 min-[720px]:max-[1340px]:mt-[120px] min-[1340px]:mt-[90px] flex flex-col">
           <header className="w-inherit flex border-b-2 border-b-light_cyan">
-              <span className= "bg-light_cyan h-[40px] w-[30px]"></span>
+              <span className= "bg-light_cyan h-[2.5rem] w-[1.875rem]"></span>
               <h1 className="font-semibold text-xl ml-4 self-center">Weather</h1>
             </header>
         
