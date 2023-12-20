d47187a356b190dee6b016de2a2eb6fe9e8d3492
Enocho1-1 <enoch.okyere@gmail.com>
2023-12-20 13:15:07 -0500

    css updates

--- src/pages/Weather/Weather.js
+++ src/pages/Weather/Weather.js
@@ -4,11 +4,11 @@ import { CurrentLocation } from "./components/CurrentLocation"
 
 export const Weather = () => {
   useTitle("WEATHER")
-
+  const responsiveCSS = "max-[720px]:mt-0 min-[720px]:max-[1340px]:mt-[120px] min-[1340px]:mt-[90px]"
  
   return (
-    <div className="flex font-open_sans">
-        <section className="w-[1200px] max-[720px]:mt-0 min-[720px]:max-[1340px]:mt-[120px] min-[1340px]:mt-[90px] flex flex-col">
+    <div className="max-w-[1200px] w-[100%] flex font-open_sans">
+        <section className={responsiveCSS} id="weather-page">
           <header className="w-inherit flex border-b-2 border-b-light_cyan">
               <span className= "bg-light_cyan h-[2.5rem] w-[1.875rem]"></span>
               <h1 className="font-semibold text-xl ml-4 self-center">Weather</h1>
