231bc4822964ba6c0f020be1f88565513c185081
Enocho1-1 <enoch.okyere@gmail.com>
2023-12-26 12:07:31 -0500

    search weather component updates

--- src/pages/Weather/SearchWeather.js
+++ src/pages/Weather/SearchWeather.js
@@ -9,13 +9,22 @@ export const SearchWeather = () => {
     useTitle(`${location}`)
     const {data:userLocation} = useSearchLocation(location)
     const { main,sys,weather,wind,} = userLocation
-    console.log(sys)
+    const { feels_like,humidity,pressure,temp} = main
+    const { country,sunrise} = sys
+    const {main: weather_main,description} = weather[0]
+    const {speed,deg,gust} = wind
+
   return (
     <div className="max-w-[1200px] w-[100%] flex font-open_sans">
         <section className="mt-[100px] max-w-inherit w-[100%]">
             <LocationWeatherForm/>
-            <header>
-                <h1 className="font-roboto text-gray-800 text-4xl mx-4">{location}</h1>
+            <header className="mt-8 flex">
+                <h1 className="font-roboto text-gray-800 text-5xl mx-4">{location.toUpperCase()},{country}</h1>
+
+                <span className=" ml-[6.25rem] flex">
+                  <h1 className="text-[2.813rem] text-orange-400 font-bold" data-testid = "temp">{Math.ceil(temp)}°F</h1>
+                  <p className="text-[35px] ml-4 text-gray-700 font-bold " data-testid = "real-feel">| {Math.ceil(feels_like)}°F</p>
+                </span>
             </header>
         
         </section>
