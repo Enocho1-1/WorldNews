d1e1d86162949d1939d7fa9a4d32128ccc81c067
Enocho1-1 <enoch.okyere@gmail.com>
2023-12-30 17:54:04 -0500

    Update BannerImage component to use CSS background-image instead of an img tag.

--- src/pages/Individual/components/BannerImage.js
+++ src/pages/Individual/components/BannerImage.js
@@ -4,8 +4,15 @@ export const BannerImage = ({image,country}) => {
  
   return (
     <div className="relative flex flex-col">
-        <span className="absolute top-0 border-l-4 flex justify-center items-center  text-sm border-dark_orange bg-white h-[50px] w-[180px]">{country}</span>
-        <img src={image} className="h-[600px] w-inherit" alt="new-img"/>
+         
+        <div className="individual-img" style={ {
+                backgroundImage: `url(${image})`,
+                backgroundSize:"cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPositionY: "50%"
+        }}>
+          <span className="absolute bottom-0 border-l-4 flex justify-center items-center  text-sm border-dark_orange bg-white h-[50px] w-[180px]">{country}</span> 
+        </div>
         <span className="mt-1 px-4 flex justify-end">
             <p className="text-xs">Copyright © worldnews/Copyright 2023 The AP. All rights reserved.</p>
         </span>
