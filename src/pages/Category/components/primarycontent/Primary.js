2dd42de4b32caacfdb676663de8ec583f3308530
Enocho1-1 <enoch.okyere@gmail.com>
2024-01-08 18:20:08 -0500

    Update FirstArticle component to use new data structure and remove unused code. (generated by blackbox.ai )

--- src/pages/Category/components/primarycontent/Primary.js
+++ src/pages/Category/components/primarycontent/Primary.js
@@ -5,23 +5,11 @@ import generic from "../../../../assets/images/world_news_2017.jpg"
 
 export const Primary = ({news,colors}) => {
     const {border,left} = colors
-    const { image_url,title,pubDate,description} = news[0]
     const primaryNews = news.slice(1,4)
 
-    const posterImage = image_url ? image_url : generic
-
-
-    const firstArticleObj = {
-        article: news[0],
-        image: posterImage,
-        title: title,
-        date: pubDate,
-        description: description,
-    }
-    
   return (
     <section className="flex w-inherit">
-        <FirstArticle object={firstArticleObj}/>
+        <FirstArticle data={news[0]}/>
 
         <aside className="ml-4 flex flex-col py-6  items-center h-[200] max-w-[500px]">
             {primaryNews.map((item,index) => (
---
+++ src/pages/Category/components/primarycontent/PrimaryArticles.js
@@ -0,0 +1,8 @@
+import { useNavigate } from "react-router-dom"
+import { navigateToArticlePage } from "../../../../utility"
+
+export const PrimaryArticles = () => {
+  return (
+    <div>PrimaryArticles</div>
+  )
+}
