56c6621b197a14f94a2fc168a6722534b72c8a7b
Enocho1-1 <enoch.okyere@gmail.com>
2024-01-08 19:25:49 -0500

    Adjust the width of the PrimaryArticles container and the height of the keyword spans in the NewsContent component. (generated by blackbox.ai )

--- src/pages/Category/components/primarycontent/PrimaryArticles.js
+++ src/pages/Category/components/primarycontent/PrimaryArticles.js
@@ -4,7 +4,7 @@ import { CategoryCard } from "../CategoryCard"
 export const PrimaryArticles = ({news,colors}) => {
     const {border,left} = colors
   return (
-    <aside className="ml-4 flex flex-col py-6  items-center  max-[1281px]:w-[90%] min-[1281px]:max-w-[500px] ">
+    <aside className="ml-4 flex flex-col py-6  items-center  max-[1281px]:w-[90%] min-[1281px]:max-w-[545px] ">
         {news.map((item,index) => (
             <CategoryCard key={index} data={item} borderColor={border} left={left}/>
         ))}
