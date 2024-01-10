09acee82fcddb60ba5492bd2fa68eb77869bed7f
Enocho1-1 <enoch.okyere@gmail.com>
2024-01-10 13:15:37 -0500

    Update UI of Category page -seconary content box-view more button

--- src/pages/Category/components/secondarycontent/Secondary.js
+++ src/pages/Category/components/secondarycontent/Secondary.js
@@ -6,8 +6,9 @@ export const Secondary = ({news,colors}) => {
     const secondaryNews = news.slice(5,lastIndex)
 
   return (
-    <section className="mt-6">
+    <section className="mt-6 flex flex-col">
         < SecondaryArticles  news={secondaryNews} colors={colors}/>
+        <button className={`${colors.color} rounded-md p-2 self-end mr-8 font-semibold text-[15px] hover:text-gray-200`}>View More <span className="bi bi-plus text-[15px] font-bold"></span></button>
     </section>
   )
 }
