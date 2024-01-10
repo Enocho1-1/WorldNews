7bca32b6d340210c260200a8f0d83180eb795d19
Enocho1-1 <enoch.okyere@gmail.com>
2024-01-10 13:30:51 -0500

    Adjust the number of columns in SecondaryArticles component - responsive design-mobile + tablet

--- src/pages/Category/components/secondarycontent/SecondaryArticles.js
+++ src/pages/Category/components/secondarycontent/SecondaryArticles.js
@@ -3,7 +3,7 @@ import { CategoryCard } from "../CategoryCard"
 export const SecondaryArticles = ({news,colors}) => {
     const {border,left} = colors
   return (
-    <aside className="px-4 py-4 grid grid-cols-twocols gap-y-2 gap-x-4">
+    <aside className="px-4 py-4 grid grid-cols-twocols gap-y-2 gap-x-4 max-lg:grid-cols-1">
          {news.map((item,index) => (
             <CategoryCard key={index} data={item} borderColor={border} left={left}/>
         ))}
