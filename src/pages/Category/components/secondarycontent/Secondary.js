a78d55ad7a81f971d4a36092a88d6dc8ae70b4ab
Enocho1-1 <enoch.okyere@gmail.com>
2024-01-10 13:21:29 -0500

    Add a "View More" button to load more secondary news articles. (generated by blackbox.ai )

--- src/pages/Category/components/secondarycontent/Secondary.js
+++ src/pages/Category/components/secondarycontent/Secondary.js
@@ -4,11 +4,11 @@ import { SecondaryArticles } from "./SecondaryArticles"
 export const Secondary = ({news,colors}) => {
     const [lastIndex,setLastIndex] = useState(15)
     const secondaryNews = news.slice(5,lastIndex)
-
+   
   return (
-    <section className="mt-6 flex flex-col">
+    <section className="mt-6 py-4 flex flex-col">
         < SecondaryArticles  news={secondaryNews} colors={colors}/>
-        <button className={`${colors.color} rounded-md p-2 self-end mr-8 font-semibold text-[15px] hover:text-gray-200`}>View More <span className="bi bi-plus text-[15px] font-bold"></span></button>
+        {secondaryNews.length === 30 ? null : (<button onClick={() => setLastIndex(prevState => prevState += 10)} className={`${colors.color} rounded-md p-2 self-end mr-8 font-semibold text-[15px] hover:text-gray-200`}>View More <span className="bi bi-plus text-[15px] font-bold"></span></button>)}
     </section>
   )
 }
