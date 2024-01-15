1161b85d478538ba4220e98cbe302ae14a0bf7a6
Enocho1-1 <enoch.okyere@gmail.com>
2024-01-14 23:13:46 -0500

    Update location details component and add a random location generator.

--- src/pages/Weather/components/Denied/DeniedAccess.js
+++ src/pages/Weather/components/Denied/DeniedAccess.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import { useState,useEffect } from "react"
+import { useSearchLocation } from "../../../../hooks"
 
 export const DeniedAccess = () => {
+    const [location,setLocation] = useState(null)
+    const locations = ["Hungary","Cambodia","Mexico","Fiji","Croatia","Congo","Ethiopia","Mozambique","Cameroon","Switzerland","Belarus","Senegal","Chad","Bahamas","Guinea-Bissau","Greece","Namibia","Japan","Georgia","Saint Vincent and the Grenadines","Andorra","Brazil","Tanzania","Ukraine","Eswatini","Mongolia","Haiti","Singapore","Barbados","Malta","Afghanistan","Trinidad and Tobago","Uganda","Equatorial Guinea"]
+    const randomLocation = arr => {
+        const randomCountry = arr[Math.floor(Math.random() * arr.length)]
+        return randomCountry 
+    }
+
   return (
     <div>DeniedAccess</div>
   )
