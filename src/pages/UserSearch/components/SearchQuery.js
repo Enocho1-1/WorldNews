9f174396b87649cb10de403056cba7756137f79f
Enocho1-1 <enoch.okyere@gmail.com>
2024-01-18 14:06:20 -0500

    Replace `GifLoader` with `NotFound` component when search results are empty.

--- src/pages/UserSearch/components/SearchQuery.js
+++ src/pages/UserSearch/components/SearchQuery.js
@@ -1,8 +1,9 @@
 import { useState } from "react"
 import { useNewsSearch } from "../../../hooks/useNewsSearch"
+import { NotFound } from "./NotFound"
 import { SearchQueryCard } from "./SearchQueryCard"
 import { Pagination } from "./Pagination"
-import {GifLoader} from "../../../components"
+
 
 export const SearchQuery = ({query}) => {
   const {data} = useNewsSearch(query)
@@ -17,9 +18,10 @@ export const SearchQuery = ({query}) => {
   }
   return (
     <div className="my-8 px-8 flex flex-col max-[769px]:items-center font-open_sans">
-      {data.length === 0 ? <GifLoader height="500px" /> : queryData.map((item,index) => (
+      {!data.length ? <NotFound/> : queryData.map((item,index) => (
          <SearchQueryCard key={index} data={item}/>
       )) }
+   
       <Pagination data={data} postperPage={postperPage} pageChange={pageChange}/>
     </div>
   )
