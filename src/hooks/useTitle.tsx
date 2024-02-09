82455a25bee6a56450604596dba868ba9a9f664b
Enocho1-1 <enoch.okyere@gmail.com>
2024-02-09 11:40:24 -0500

    update the `useTitle` hook.

--- src/hooks/useTitle.tsx
+++ src/hooks/useTitle.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react"
 
-export const useTitle = (title) => {
+export const useTitle = (title:string) => {
     useEffect(() => {
         document.title = `${title} - Daily News, hot news and breaking News in the world`;
     },[title])
