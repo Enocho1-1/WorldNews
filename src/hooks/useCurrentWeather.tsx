e1688fda19ddd120af3d88c8cdfab694bc00ac63
Enocho1-1 <enoch.okyere@gmail.com>
2024-02-13 13:01:36 -0500

    Update useCurrentWeather hook - change naming convention for  currentLocationWeather parameter

--- src/hooks/useCurrentWeather.tsx
+++ src/hooks/useCurrentWeather.tsx
@@ -3,6 +3,7 @@ import { useState,useEffect } from "react";
 import { currentLocationWeather } from "../utility";
 
 interface WeatherDetail{clouds:number,dew_point:number,dt:number,feels_like:number,humidity:number,pop:number,pressure:number,temp:number,uvi:number,visibility:number,weather:{id:number,main:string,description:string}[],wind_deg:number,wind_gust:number,wind_speed:number}
+
 export interface CurrentWeatherApi{
   current: WeatherDetail;
   daily:{dt:number, sunrise:number,sunset:number}[];
@@ -15,13 +16,8 @@ export interface CurrentWeatherApi{
 }
 
 export const useCurrentWeather = () => {
+
     const [main,setMain] = useState<CurrentWeatherApi | null>(null)
-    // const [condition,setCondition] = useState("")
-    // const [hourly,setHourly] = useState([])
-    // const [sun,setSun] = useState("")
-    // const [wind,setWind] = useState("")
-    // const [temp,setTemp] = useState("")
-    // const [feels,setFeels] = useState("")
     const [fulfilled,setFulfilled] = useState(false)
     const [deniedAccess,setDeniedAccess] = useState(false)
 
@@ -32,23 +28,18 @@ export const useCurrentWeather = () => {
       
       function success(pos: { coords: any; }) {
         const crd = pos.coords;
+        
         sessionStorage.setItem("latitude",JSON.stringify(crd.latitude))
         sessionStorage.setItem("longitude",JSON.stringify(crd.longitude)) 
 
-        const currentWeatherObj = {
+        const currentLocation = {
           latitude:crd.latitude,
           longitude:crd.longitude,
           setMain:setMain,
-          // setCondition:setCondition,
-          // setHourly:setHourly,
-          // setSun:setSun,
-          // setWind:setWind,
-          // setTemp:setTemp,
-          // setFeels:setFeels,
           setFulfilled:setFulfilled
         }
         
-        currentLocationWeather(currentWeatherObj);
+        currentLocationWeather(currentLocation);
       }
       
       function error(err: { code: any; message: any; }) {
